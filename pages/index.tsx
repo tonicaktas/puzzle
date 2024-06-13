307dcdd9764e8265c61cce2e58c10dcfac27c38a
toni caktas <tonicaktas@tonis-MacBook-Pro.local>
2024-06-13 02:26:31 +0200

    responsivness-v3

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,7 @@ const Hem: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-50 p-8">
+    <div className="flex flex-col items-center justify-center bg-gray-50 p-8 h-full min-h-screen">
       {!arSpelStartat && (
         <>
           <h1 className="text-3xl mb-6">Pussel Spel</h1>
